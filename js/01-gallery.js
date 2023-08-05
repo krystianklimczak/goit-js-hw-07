68970c56c75ed2f03142c9b94ae9ed4e3e471b9a
krystianklimczak <krystianklimczak1996r.com>
2023-08-05 17:01:34 +0200

    completed first ex

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryArray = [];
+
+galleryItems.forEach(img => {
+  const galleryElement = document.createElement('li');
+  galleryElement.classList.add('gallery__item');
+  const imageContent = `
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+`;
+  galleryElement.insertAdjacentHTML('afterbegin', imageContent);
+  galleryArray.push(galleryElement);
+});
+
+gallery.append(...galleryArray);
+
+const galleryItem = document.querySelectorAll('.gallery__item');
+
+galleryItem.forEach(el => {
+  el.addEventListener('click', event => {
+    event.preventDefault();
+    const instance = basicLightbox
+      .create(
+        `
+    <img src="${event.target.dataset.source}" />`,
+        {
+          onShow: instance => {
+            const handler = ev => {
+              if (ev.key === 'Escape') {
+                instance.close();
+                document.removeEventListener('keydown', handler);
+              }
+            };
+            document.addEventListener('keydown', handler);
+          },
+        }
+      )
+      .show();
+  });
+});
