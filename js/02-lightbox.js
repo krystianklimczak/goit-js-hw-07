7ea8c12e959d120d7575233627da80f7abc03e41
krystianklimczak <krystianklimczak1996r.com>
2023-08-06 13:48:07 +0200

    completed second ex

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryArray = [];
+
+galleryItems.forEach(img => {
+  const galleryElement = document.createElement('li');
+  //   galleryElement.classList.add('gallery__item');
+  const imageContent = `
+  <a class="gallery__item" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      alt="${img.description}"
+      
+    />
+  </a>
+`;
+  galleryElement.insertAdjacentHTML('afterbegin', imageContent);
+  galleryArray.push(galleryElement);
+});
+
+gallery.append(...galleryArray);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
